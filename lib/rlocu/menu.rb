4c8bedeab09bdd91092245aabf9fec85b80b67dd
Stephen Philp <stephen@shelf.com>
2014-03-27 15:02:35 -0700

    move subsections, options etc under Menu class

--- lib/rlocu/menu.rb
+++ lib/rlocu/menu.rb
@@ -38,112 +38,111 @@ module Rlocu
       str
     end
 
-  end
+    class Section
+      attr_accessor :name
+      attr_reader :subsections
 
-  class Section
-    attr_accessor :name
-    attr_reader :subsections
+      def initialize(meta_section)
+        @name = meta_section['section_name']
+        self.subsections = meta_section['subsections']
+      end
 
-    def initialize(meta_section)
-      @name = meta_section['section_name']
-      self.subsections = meta_section['subsections']
+      def subsections=(meta_subsections)
+        @subsections = []
+        meta_subsections.each {|h| @subsections << Subsection.new(h)}
+      end
+
     end
 
-    def subsections=(meta_subsections)
-      @subsections = []
-      meta_subsections.each {|h| @subsections << Subsection.new(h)}
-    end
+    class Subsection
+      attr_accessor :name
+      attr_reader :contents
 
-  end
+      def initialize(meta_subsection)
+        @name = meta_subsection['subsection_name']
+        self.contents = meta_subsection['contents']
+      end
 
-  class Subsection
-    attr_accessor :name
-    attr_reader :contents
-
-    def initialize(meta_subsection)
-      @name = meta_subsection['subsection_name']
-      self.contents = meta_subsection['contents']
-    end
-
-    def contents=(meta_contents)
-      @contents = []
-      meta_contents.each do |h|
-        #TODO: raise error if type is different
-        @contents << case h['type']
-          when 'SECTION_TEXT'
-            SectionText.new(h)
-          when 'ITEM'
-            MenuItem.new(h)
-        end  
+      def contents=(meta_contents)
+        @contents = []
+        meta_contents.each do |h|
+          #TODO: raise error if type is different
+          @contents << case h['type']
+            when 'SECTION_TEXT'
+              SectionText.new(h)
+            when 'ITEM'
+              MenuItem.new(h)
+          end  
+        end
       end
     end
-  end
 
-  class SectionText
-    attr_accessor :type, :text
+    class SectionText
+      attr_accessor :type, :text
 
-    def initialize(meta_section_text)
-      @type = meta_section_text['type']
-      @text = meta_section_text['text']
+      def initialize(meta_section_text)
+        @type = meta_section_text['type']
+        @text = meta_section_text['text']
+      end
+
+      def to_s
+        @text
+      end
     end
 
-    def to_s
-      @text
-    end
-  end
+  # create a to_s for each that can be over-ridden by the user
+    class MenuItem
+      attr_accessor :type, :name, :description, :price
+      attr_reader :option_groups
 
-# create a to_s for each that can be over-ridden by the user
-  class MenuItem
-    attr_accessor :type, :name, :description, :price
-    attr_reader :option_groups
+      def initialize(meta_menu_item)
+        @type = meta_menu_item['type']
+        @name = meta_menu_item['name']
+        @description = meta_menu_item['description']
+        @price = meta_menu_item['price']
+        self.option_groups = meta_menu_item['option_groups'] 
+      end
 
-    def initialize(meta_menu_item)
-      @type = meta_menu_item['type']
-      @name = meta_menu_item['name']
-      @description = meta_menu_item['description']
-      @price = meta_menu_item['price']
-      self.option_groups = meta_menu_item['option_groups'] 
-    end
+      def option_groups=(meta_option_groups)
+        @option_groups = []
+        meta_option_groups.each { |h| @option_groups << OptionGroup.new(h) } if meta_option_groups
+      end
 
-    def option_groups=(meta_option_groups)
-      @option_groups = []
-      meta_option_groups.each { |h| @option_groups << OptionGroup.new(h) } if meta_option_groups
     end
 
-  end
+    class OptionGroup
+      attr_accessor :type, :text
+      attr_reader :options
 
-  class OptionGroup
-    attr_accessor :type, :text
-    attr_reader :options
+      def initialize(meta_option_group)
+        @type = meta_option_group['type']
+        @text = meta_option_group['text']
+        self.options = meta_option_group['options'] 
+      end
 
-    def initialize(meta_option_group)
-      @type = meta_option_group['type']
-      @text = meta_option_group['text']
-      self.options = meta_option_group['options'] 
+      def options=(meta_options)
+        @options = []
+        meta_options.each {|h| @options << Option.new(h) } if meta_options
+      end
     end
 
-    def options=(meta_options)
-      @options = []
-      meta_options.each {|h| @options << Option.new(h) } if meta_options
-    end
-  end
+    class Option
+      attr_accessor :name, :price
 
-  class Option
-    attr_accessor :name, :price
+      def initialize(meta_option)
+        @name = meta_option['name']
+        @price = meta_option['price']
+      end
 
-    def initialize(meta_option)
-      @name = meta_option['name']
-      @price = meta_option['price']
-    end
+      def price
+        # price can be relative indicated by a + in the price
+        # not currently doing anything w it 
+        @price
+      end
 
-    def price
-      # price can be relative indicated by a + in the price
-      # not currently doing anything w it 
-      @price
-    end
-
-    def to_s
-      "#{name} #{price}"
+      def to_s
+        "#{name} #{price}"
+      end
     end
   end
 end
