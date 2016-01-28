cac626ca15770bd972b9394c915e347cbce01b92
Stephen Philp <stephen@shelf.com>
2016-01-28 13:59:04 -0800

    Remove The Old Tests

--- test/test_menu.rb
+++
@@ -1,8 +0,0 @@
-class MenuTest < MiniTest::Unit::TestCase
-  include Rlocu
-
-  def test_options
-    meta_options_hash = {'name' => "Chicken"}
-    assert_equal "Chicken ", Rlocu::Menu::Option.new(meta_options_hash).to_s
-  end
-end
--- test/test_truth.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-
-class TestTruth < MiniTest::Unit::TestCase
-  def test_reality
-    assert true, "aye, it is true"
-  end
-end
