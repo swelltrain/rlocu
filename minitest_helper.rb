6914c06dc1ae3996565ef7fada62a8c9a6fc0ebe
Stephen Philp <stephen@shelf.com>
2016-01-28 14:10:05 -0800

    remove minitest_helper

--- minitest_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require_relative 'lib/rlocu'
-# run some code before every test so we don't 
-# have to specify setup in each test file
-MiniTest::Unit::TestCase.add_setup_hook { Rlocu.config! File.expand_path("./.config") }
-
-Dir.glob('./test/test_*.rb').each { |file| require file}
-
