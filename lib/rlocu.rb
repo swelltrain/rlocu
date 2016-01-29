1582c52c8dd8ad24e0e1424a55d9a4441dce4efe
Stephen Philp <stephen@shelf.com>
2016-01-28 21:29:53 -0800

    Rlocu.api_key

--- lib/rlocu.rb
+++ lib/rlocu.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 module Rlocu
   RlocuError = Class.new(StandardError)
   class << self
+    attr_accessor :api_key
     def configure
       yield self
     end
@@ -21,3 +22,6 @@ end
 require_relative 'rlocu/version'
 require_relative 'rlocu/utilities/location'
 require_relative 'rlocu/utilities/bounds'
+require_relative 'rlocu/query'
+require_relative 'rlocu/venue_query'
+require_relative 'rlocu/menu_item_query'
