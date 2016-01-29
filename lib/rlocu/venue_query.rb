95850278638d3021c4e4347b12d586b8536eab76
Stephen Philp <stephen@shelf.com>
2016-01-28 21:28:57 -0800

    Rlocu::VenueQuery#new
      should raise ArgumentError unless return_fields is an array

--- lib/rlocu/venue_query.rb
+++ lib/rlocu/venue_query.rb
@@ -2,6 +2,7 @@ module Rlocu
   class VenueQuery
     def initialize(query_fields:, return_fields:)
       raise ArgumentError, 'Query Fields Param must be a hash of fields: values.' unless query_fields.is_a? Hash
+      raise ArgumentError, 'Return Fields Param must be an array of fields.' unless return_fields.is_a? Array
     end
   end
 end
--- spec/venue_query_spec.rb
+++ spec/venue_query_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe VenueQuery, '#new' do
     expect{ VenueQuery.new(query_fields: 'spec', return_fields: ['spec']) }.to raise_error(ArgumentError)
   end
 
-  it 'should raise ArgumentError unless return_fields is an array'
+  it 'should raise ArgumentError unless return_fields is an array' do
+    expect{ VenueQuery.new(query_fields: {field: 'spec'}, return_fields: 'spec') }.to raise_error(ArgumentError)
+
+  end
 
   it 'should raise ArgumentError if return_fields is empty'
 
