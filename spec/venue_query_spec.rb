38e5a672c997cd2716f8b3e5f9f4aa93d539602b
Stephen Philp <stephen@shelf.com>
2016-01-28 21:45:38 -0800

    Rlocu::VenueQuery#new
      should raise ArgumentError if return_fields is empty

--- spec/venue_query_spec.rb
+++ spec/venue_query_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe VenueQuery, '#new' do
 
   it 'should raise ArgumentError unless return_fields is an array' do
     expect{ VenueQuery.new(query_fields: {field: 'spec'}, return_fields: 'spec') }.to raise_error(ArgumentError)
-
   end
 
-  it 'should raise ArgumentError if return_fields is empty'
+  it 'should raise ArgumentError if return_fields is empty' do
+    expect{ VenueQuery.new(query_fields: {field: 'spec'}, return_fields: []) }.to raise_error(ArgumentError)
+  end
 
   it 'should raise ArgumentError if query_fields is empty'
 end
