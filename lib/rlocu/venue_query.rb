e836e553e9a436ca998ade68e7dea5a1a1da3b6b
Stephen Philp <stephen@shelf.com>
2016-02-01 08:02:32 -0800

    VenueQuery
      set the instance vars

--- lib/rlocu/venue_query.rb
+++ lib/rlocu/venue_query.rb
@@ -1,9 +1,12 @@
 module Rlocu
   class VenueQuery
+    attr_reader :query_fields, :return_fields
     def initialize(query_fields:, return_fields:)
       raise ArgumentError, 'Query Fields Param must be a hash of fields: values.' if !query_fields.is_a?(Hash) || query_fields.empty?
 
       raise ArgumentError, 'Return Fields Param must be an array of fields.' if !return_fields.is_a?(Array) || return_fields.empty?
+      @query_fields = query_fields
+      @return_fields = return_fields
     end
   end
 end
