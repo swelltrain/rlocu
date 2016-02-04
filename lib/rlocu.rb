64e52908f6c6a3e6083f4c96ec6809fa6cc3f2ab
Stephen Philp <stephen@shelf.com>
2016-02-04 11:28:06 -0800

    Remove Rlocu#encode
      no longer needed since we are using rest-client for api v2

--- lib/rlocu.rb
+++ lib/rlocu.rb
@@ -1,4 +1,3 @@
-require 'open-uri'
 
 module Rlocu
   RlocuError = Class.new(StandardError)
@@ -10,11 +9,6 @@ module Rlocu
 
     def api_version
       'v2'
-    end
-
-    def encode(params={})
-      s = params.reduce("api_key=#{Rlocu.api_key}&") {|memo,(key,val)| memo << "#{key.to_s}=#{val}&"}
-      URI::encode(s.chop)
     end
   end
 end
