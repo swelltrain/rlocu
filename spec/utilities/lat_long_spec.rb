96e2583805bb57c31f84b328b67e7850738e7685
Stephen Philp <stephen@shelf.com>
2016-02-06 11:14:54 -0800

    Rlocu::Utilities::LatLong#new
      should raise ArgumentError if lat param is not a float

--- spec/utilities/lat_long_spec.rb
+++ spec/utilities/lat_long_spec.rb
@@ -1,7 +1,7 @@
 include Rlocu::Utilities
 
-RSpec.describe Location, '#new' do
+RSpec.describe LatLong, '#new' do
   it 'should raise ArgumentError if lat param is not a float' do
-    expect{ Location.new(lat: '1', long: 2) }.to raise_error(ArgumentError)
+    expect{ LatLong.new(lat: '1', long: 2) }.to raise_error(ArgumentError)
   end
 end
