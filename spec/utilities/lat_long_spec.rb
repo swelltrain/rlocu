a47b105bdbc93b581117f6f5bdc3797348bf6463
Stephen Philp <stephen@shelf.com>
2016-02-08 08:40:54 -0800

    remove lat long spec

--- spec/utilities/lat_long_spec.rb
+++
@@ -1,7 +0,0 @@
-include Rlocu::Utilities
-
-RSpec.describe LatLong, '#new' do
-  it 'should raise ArgumentError if lat param is not a float' do
-    expect{ LatLong.new(lat: '1', long: 2) }.to raise_error(ArgumentError)
-  end
-end
