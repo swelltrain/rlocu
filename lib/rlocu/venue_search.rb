f434724ff1535545458b839e26e02bf04f3988d7
Stephen Philp <stephen@shelf.com>
2016-03-03 16:37:14 -0800

    Rlocu::VenueSearch#with_menus
      returns itself
    
    Rlocu::VenueSearch#locu_id
    
      returns itself
    
    Rlocu::VenueSearch#in_lat_long_radius
    
      returns itself

---
+++ lib/rlocu/venue_search.rb
@@ -0,0 +1,34 @@
+module Rlocu
+  class VenueSearch
+    def initialize(return_fields: [])
+      @return_fields = return_fields.empty? ? %w{locu_id name location description menus} : return_fields
+      @key_value_conditions = []
+      @venue_query = nil
+    end
+
+    def with_menus
+      key_value_conditions << QueryBuilder::KeyValueCondition.new(key: 'menus', value: true, condition: '$present')
+      self
+    end
+
+    def in_lat_long_radius(lat:, long:, radius:)
+      lat_long_radius = Utilities::LatLongRadius.new(lat: lat, long: long, radius: radius)
+      key_value_conditions << QueryBuilder::KeyValueCondition.new(key: 'location', value: lat_long_radius, condition: '$in_lat_lng_radius')
+      self
+    end
+
+    def locu_id(locu_id)
+      key_value_conditions = [QueryBuilder::KeyValueCondition.new(key: 'locu_id', value: locu_id)]
+      self
+    end
+
+    def search
+      conditions = [QueryBuilder::QueryCondition.new(key_value_conditions: key_value_conditions)]
+      venue_query = VenueQuery.new(query_conditions: conditions, return_fields: return_fields)
+      venue_query.query
+    end
+
+    private
+    attr_reader :return_fields, :key_value_conditions
+  end
+end
---
+++ spec/venue_search_spec.rb
@@ -0,0 +1,24 @@
+include Rlocu
+require 'config_rlocu_spec_helper'
+
+RSpec.describe VenueSearch, '#with_menus' do
+  it 'returns itself' do
+    venue_search = VenueSearch.new
+    expect(venue_search.with_menus).to eq(venue_search)
+  end
+end
+
+
+RSpec.describe VenueSearch, '#in_lat_long_radius' do
+  it 'returns itself' do
+    venue_search = VenueSearch.new
+    expect(venue_search.in_lat_long_radius(lat: 1, long: 1, radius: 1)).to eq(venue_search)
+  end
+end
+
+RSpec.describe VenueSearch, '#locu_id' do
+  it 'returns itself' do
+    venue_search = VenueSearch.new
+    expect(venue_search.locu_id('spec')).to eq(venue_search)
+  end
+end
