63577622a33141a37a34adcea214c5dbe3de432a
Stephen Philp <stephen@shelf.com>
2016-03-07 07:50:50 -0800

    GeoJSON
      Latitude and longitude

--- lib/rlocu/geo_json.rb
+++ lib/rlocu/geo_json.rb
@@ -4,6 +4,18 @@ module Rlocu
       raise ArgumentError unless geo['type']
       raise ArgumentError unless geo['coordinates']
       @point = geo['type']
+      @coordinates = geo['coordinates']
     end
+
+    def longitude
+      coordinates[0]
+    end
+
+    def latitude
+      coordinates[1]
+    end
+
+    private
+    attr_reader :coordinates
   end
 end
